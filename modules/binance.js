b602a7eeb44460d4ab1b6225c9e3215eb2159a5d
Giovanni Putignano <gputignano@gmail.com>
2023-12-03 18:19:23 +0100

    Don't need to import grid value

--- modules/binance.js
+++ modules/binance.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import path from "path";
 import _ from "lodash";
 import * as dotenv from "dotenv";
-import { grid } from "./argv.js";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 
